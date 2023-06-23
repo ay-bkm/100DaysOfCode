dcda95e36bfc2fad9664b5c6fe05a745a7e1372c
ay-bkm <ay.belkacem@gmail.com>
2023-06-23 02:25:59 +0100

    Odd Or Even

--- Day 03/OddOrEven.py
+++ Day 03/OddOrEven.py
@@ -1,6 +1,5 @@
 number = int(input("which number do you want to check?"))
-modulus = number % 2
-if modulus == 0:
+if number % 2 == 0:
     print("This is an Even Number!")
 else:
     print("This is an Odd Number!")
