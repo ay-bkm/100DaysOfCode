3e16333920a3673568ea0b2026f7ac6aad0a4d0e
ay-bkm <ay.belkacem@gmail.com>
2023-07-04 19:03:04 +0100

    Just Commiting

--- Day 07/Hangman_part_01.py
+++ Day 07/Hangman_part_01.py
@@ -2,7 +2,7 @@ import random
 word_list = ["sweater", "vexillology", "goverment"]
 
 chosen_word = random.choice(word_list)
-guess = input("Guess a letter: \n").lower()
+guess = input("Guess a letter: ").lower()
 for char in chosen_word:
     if guess == char:
         print("right")
