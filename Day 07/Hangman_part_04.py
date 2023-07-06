54b3bdea96934f96351c7e83a8d709268be4c689
ay-bkm <ay.belkacem@gmail.com>
2023-07-06 20:26:53 +0100

    Yaaaaaay!

---
+++ Day 07/Hangman_part_04.py
@@ -0,0 +1,86 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+lives = 6
+
+word_list = ["tent", "autobus", "insurance", "execute"]
+chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}')
+
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+while "_" in display and lives > 0:
+    guess = input("Guess a letter: ")
+    found = False
+    for index in range(len(chosen_word)):
+        
+        if chosen_word[index] == guess:
+            display[index] = guess
+            found = True
+        
+    if found == False:
+        lives -= 1
+        print(stages[lives])
+            
+    print(display) 
+if lives == 0:
+    print("You lose!")
+else:
+    print("You win!")
