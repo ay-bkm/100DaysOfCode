4d523335bb85567e1dcacaeb7153fb785e2fcb90
ay-bkm <ay.belkacem@gmail.com>
2023-07-06 21:08:45 +0100

    Ayyyyyyy!

---
+++ Day 07/Hangman_part_05-directory/Hangman_part_05.py
@@ -0,0 +1,39 @@
+import random
+import stages
+import word_list
+
+lives = 6
+
+
+chosen_word = random.choice(word_list.word_list)
+print(f'Pssst, the solution is {chosen_word}')
+
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+    
+while "_" in display and lives > 0:
+    guess = input("Guess a letter: ")
+    found = False
+    if guess in display:
+            print(f"You've already guessed the letter {guess}")
+    for index in range(len(chosen_word)):
+        
+        if chosen_word[index] == guess:
+            display[index] = guess
+            found = True
+        
+        
+    if guess not in chosen_word:
+             print(f"{guess} don't exist in the word.")    
+    if found == False:
+        lives -= 1
+        print(stages.stages[lives])
+    
+            
+    print(f"{' '.join(display)}")
+if lives == 0:
+    print("You lose!")
+else:
+    print("You win!")
---
+++ Day 07/Hangman_part_05-directory/word_list.py
@@ -0,0 +1,20 @@
+word_list = [
+    "tent", "autobus", "insurance", "execute", "computer", "keyboard", "cat", "dog", "table", "chair",
+    "pencil", "book", "car", "bicycle", "tree", "flower", "sun", "moon", "star", "planet", "music",
+    "guitar", "piano", "violin", "painting", "artist", "movie", "actor", "actress", "camera", "phone",
+    "house", "building", "city", "country", "ocean", "beach", "mountain", "river", "bridge", "road",
+    "train", "plane", "ship", "helicopter", "school", "university", "teacher", "student", "doctor", "nurse",
+    "engineer", "scientist", "writer", "poet", "chef", "restaurant", "coffee", "tea", "milk", "juice",
+    "pizza", "burger", "salad", "cake", "chocolate", "ice cream", "football", "basketball", "tennis", "swimming",
+    "volleyball", "baseball", "golf", "soccer", "hockey", "cricket", "rugby", "running", "cycling", "hiking",
+    "gymnastics", "yoga", "meditation", "fishing", "camping", "paintball", "skiing", "snowboarding", "surfing", "diving",
+    "photography", "drawing", "gardening", "cooking", "travelling", "reading", "writing", "singing", "dancing", "acting",
+    "listening", "watching", "laughing", "thinking", "dreaming", "exploring", "learning", "playing", "creating", "relaxing",
+    "adventure", "friendship", "love", "happiness", "success", "peace", "hope", "kindness", "gratitude", "courage",
+    "patience", "wisdom", "creativity", "passion", "imagination", "perseverance", "confidence", "forgiveness", "generosity", "humility",
+    "integrity", "justice", "loyalty", "resilience", "empathy", "curiosity", "equality", "innovation", "optimism", "self-care",
+    "harmony", "balance", "serenity", "tranquility", "inspiration", "motivation", "ambition", "celebration", "achievement", "growth",
+    "challenge", "change", "knowledge", "wisdom", "freedom", "truth", "beauty", "nature", "universe", "galaxy",
+    "exploration", "discovery", "adventure", "wonder", "magic", "mystery", "imagination", "dreams", "fantasy", "journey"
+    # Add more words here...
+]
